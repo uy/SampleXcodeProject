31b8eba08bba318c4a2368e8ad29b5984ba29ca7
Utku Yeğen <utkuyegen@gmail.com>
2025-08-04 00:16:29 +0300

    Initial Commit

---
+++ SampleXcodeProject/SampleXcodeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleXcodeProjectApp.swift
+//  SampleXcodeProject
+//
+//  Created by Utku Yeğen on 4.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SampleXcodeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
